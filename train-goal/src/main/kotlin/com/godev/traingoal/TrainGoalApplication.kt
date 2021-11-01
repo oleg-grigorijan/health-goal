8eff9dae8e80db95b4006cf27712e21734936a57
Oleg Grigorijan <oleg.grigorijan@gmail.com>
2021-11-01 11:34:21 +0300

    TG: Initial commit

---
+++ train-goal/src/main/kotlin/com/godev/traingoal/TrainGoalApplication.kt
@@ -0,0 +1,11 @@
+package com.godev.traingoal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrainGoalApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrainGoalApplication>(*args)
+}
